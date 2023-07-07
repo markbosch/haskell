932c8c25fae5766007ff689ac39cc5cc7a5001e2
Mark van den Bosch <markvandenbosch@gmail.com>
2023-07-08 00:07:01 +0200

    List comprehensions exercises

--- listcomp.hs
+++ listcomp.hs
@@ -123,3 +123,84 @@ encode n xs = [shift n x | x <- xs ]
 -- > encode 3 "haskell is fun"
 -- "kdvnhoo lv ixq"
 
+-- Frequency tables
+table :: [Float]
+table = [8.1, 1.5, 2.8, 4.2, 12.7 ,2.2, 2.0, 6.1, 7.0,
+         0.2, 0.8, 4.0, 2.4, 6.7, 7.5, 1.9, 0.1, 6.0,
+         6.3, 9.0, 2.8, 1.0, 2.4, 0.2, 2.0, 0.1]
+
+percent :: Int -> Int -> Float
+percent n m = (fromIntegral n / fromIntegral m) * 100
+
+freqs :: String -> [Float]
+freqs xs = [percent (count x xs) n | x <- ['a'..'z']]
+           where n = lowers xs
+-- > freqs "aaabbbbbccccccccdddddeeeeeeeee"
+
+-- Crakcing the cipher
+-- compare the list based on the `chi-square statistic`
+
+-- os = observed frequencies
+-- es = expected frequencies
+chisqr :: [Float] -> [Float] -> Float
+chisqr os es = sum [((o-e)^2)/e | (o,e) <- zip os es]
+
+rotate :: Int -> [a] -> [a]
+rotate n xs = drop n xs ++ take n xs
+
+crack :: String -> String
+crack xs = encode (-factor) xs                                  -- encode negative
+  where
+    factor = head (positions (minimum chitab) chitab)           -- Determine the factor which is used to encode
+    chitab = [chisqr (rotate n table') table | n <- [0..25]]    
+    table' = freqs xs
+
+-- Exercises
+
+-- 1. give an expression that calc. the sum 1^2 + 2^2+... 100^2
+-- [sum [x^2} | x <- [1..100]]
+
+-- 2. make a grid of size m x n
+-- gird 1 2 -> [(0,0),(0,1),(0,2),(1,0)(1,1)(1,2)]
+
+grid :: Int -> Int -> [(Int, Int)]
+grid m n = [(x,y) | x <- [0..m], y <- [0..n]]
+
+-- 3. define a function square which utilize the grid function that returns a coordinate square of size n
+--    excluding the (0,0) to (n,n)
+-- square 2 --> [(0,1),(0,2),(1,0),(1,2),(2,0),(2,1)]
+
+square :: Int -> [(Int,Int)]
+square n = [(x,y) | (x,y) <- grid n n, x /= y]  -- create a list of pairs (x,y) drawn from grid where x is not equal to y
+
+-- 4. replicate n times the value of a
+replicate :: Int -> a -> [a]
+replicate n a = [a | _ <- [1..n]]
+
+-- 5. Pythagorean
+pyths :: Int -> [(Int,Int,Int)]
+pyths n = [(x,y,z) | x <- [1..n],
+                     y <- [1..n],
+                     z <- [1..n],
+                     x^2 + y^2 == z^2]
+
+-- 6. Perfects
+perfect :: Int -> Bool
+perfect n = sum (init(factors n)) == n
+
+perfects :: Int -> [Int]
+perfects n = [x | x <- [1..n], perfect x] 
+
+-- 7. show how the list comp. [(x,y) | x <- [1,2], y <- [3,4]] can be re-expressed using two list comprehensions with one generator
+-- contact [[(x,y) | x <- [1,2]] | y <- [3,4]]
+
+-- 8. redefine the function positions with find
+positions' :: Eq a => a -> [a] -> [Int]
+positions' x xs = [i | (x',i) <- zip xs [0..], x == x']
+
+-- 9. scalar product
+sp :: [Int] -> [Int] -> Int
+sp xs ys = sum [xs !! i * ys !! i | i <- [0..n-1]]
+           where n = Prelude.length xs
+
+
