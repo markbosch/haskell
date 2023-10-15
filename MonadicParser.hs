c9870953e0e98932bb96145c0f6635b7874ced73
Mark van den Bosch <markvandenbosch@gmail.com>
2023-10-15 22:47:32 +0200

    Monadic parsing

--- MonadicParser.hs
+++ MonadicParser.hs
@@ -84,3 +84,156 @@ three = do x <- item
 
 -- > parse three "spam"
 -- [(('s','a'),"m")]
+
+-- Choices
+
+-- Alternative -> empty or <|>
+-- this could also be done via MonadPlus from Control.Monad
+-- with mzero and mplus
+instance Alternative Parser where
+  -- empty :: Parser a
+  empty = P (\inp -> [])
+  -- (<|>) :: Parser a -> Parser a -> Parser a
+  p <|> q = P (\inp -> case parse p inp of
+                          []        -> parse q inp
+                          [(v,out)] -> [(v,out)])
+
+-- > parse empty "abc"
+-- []
+-- > parse (item <|> return 'd') "abc"
+-- [('a',"bc")]
+-- > parse (empty <|> return 'd') "abc"
+-- [('d',"abc")]
+
+-- Building some useful parsers with the above building
+-- blocks icw primitives
+
+sat :: (Char -> Bool) -> Parser Char
+sat p = do x <- item
+           if p x then return x else empty
+
+-- digit
+digit :: Parser Char
+digit = sat isDigit
+
+-- lower
+lower :: Parser Char
+lower = sat isLower
+
+-- upper
+upper :: Parser Char
+upper = sat isUpper
+
+-- letter
+letter :: Parser Char
+letter = sat isAlpha
+
+-- alphanum
+alphanum :: Parser Char
+alphanum = sat isAlpha
+
+-- char
+char :: Char -> Parser Char
+char x = sat (== x)
+
+-- > parse (char 'a') "abc"
+-- [('a',"bc")]
+
+-- string
+string :: String -> Parser String
+string []     = return []
+string (x:xs) = do char x
+                   string xs
+                   return (x:xs)
+
+-- > parse (string "abc") "abcdefg"
+-- [("abc","defg")]
+-- > parse (string "abc") "abcdefg"
+-- [("abc","defg")]
+
+-- `many` and `some` parsers
+-- apply a parser as many times as possible untils it fails
+-- where the difference between many and some is that many
+-- permits zero or more applications while some requires
+-- at least one successfull application
+
+-- The `many` and `some` are already defined in the
+-- class Applicative f => Alternative f where
+--   many :: f a -> f [a]
+--   some :: f a -> f [a]
+
+-- > parse (many digit) "123abc"
+-- [("123","abc")]
+-- > parse (many digit) "abc"
+-- [("","abc")]
+-- > parse (some digit) "abc"
+-- []
+
+-- identifier (variable names)
+ident :: Parser String
+ident = do x  <- lower
+           xs <- many alphanum
+           return (x:xs)
+
+-- natural number
+nat :: Parser Int
+nat = do xs <- some digit
+         return (read xs)
+
+-- space
+space :: Parser ()
+space = do many (sat isSpace)
+           return ()
+
+-- > parse ident "abc def"
+-- [("abc"," def")]
+
+-- > parse nat "123 abc"
+--[(123," abc")]
+
+-- > parse space "    abc"
+-- [((),"abc")]
+
+-- Integer
+int :: Parser Int
+int = do char '-'
+         n <- nat
+         return (-n)
+       <|> nat  
+
+-- > parse int "-3 abc"
+-- [(-3," abc")]
+
+-- Handling spacing
+token :: Parser a -> Parser a
+token p = do space
+             v <- p
+             space
+             return v
+
+identifier :: Parser String
+identifier = token ident
+
+natural :: Parser Int
+natural = token nat
+
+integer :: Parser Int
+integer = token int
+
+symbol :: String -> Parser String
+symbol xs = token (string xs)
+
+nats :: Parser [Int]
+nats = do symbol "["
+          n  <- natural
+          ns <- many (do symbol ","
+                         natural)
+          symbol "]"
+          return (n:ns)
+
+-- parse nats " [1, 2, 3] "
+-- [([1,2,3],"")]
+-- > parse nats "[1, 2,]"
+-- []
+
+        
